189269f3ef1729580af3b8f61933dc898ea69c55
lincoln ingaroca <lincolningaroca@gmail.com>
2026-01-14 20:56:33 -0500

    Fix gitignore conflicts and remove ignored files

--- build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_xxxApp_FILE D:/xxxapp/xxxApp/build/Desktop_Qt_6_8_3_MinGW_64_bit-Debug/xxxApp.exe)
-set(__QT_DEPLOY_TARGET_xxxApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_xxxApp_RUNTIME_DLLS C:/Qt/6.8.3/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Network.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.3/mingw_64/bin/Qt6Core.dll)
